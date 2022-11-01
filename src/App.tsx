b52715302362c79fc1b5d51780c6fa45e17cc98d
Eric Schoen <ejschoen@i2kconnect.com>
2022-11-01 14:10:20 -0500

    Add i2k Logo overlay

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ import IconButton from '@mui/material/IconButton';
 import {SvgIcon} from "@mui/material";
 import {FormGroup, FormControlLabel, Switch} from "@mui/material";
 import {ReactComponent as NodeIcon} from "./node.svg";
+import {ReactComponent as I2KIcon} from "./i2k.svg";
+import {ReactComponent as I2KConnectIcon} from "./i2kconnect.svg";
 import {PodRequests} from "./PodRequests";
+
 
 export default function ButtonAppBar() {
     const [graphical, setGraphical] = React.useState(false);
@@ -25,7 +28,7 @@ export default function ButtonAppBar() {
                 aria-label="menu"
                 sx={{ mr: 2 }}
             >
-                <SvgIcon><NodeIcon className="Node=Logo" /></SvgIcon>
+                <SvgIcon><NodeIcon  /></SvgIcon>
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Cluster Resource Requests
@@ -42,6 +45,29 @@ export default function ButtonAppBar() {
         </AppBar>
           <Box component="main">
               <PodRequests graphical={graphical}/>
+              <div style={{position: "absolute", bottom: 48, right: 48, width: 96, height: 96}}>
+                  <IconButton size="large"
+                              edge="start"
+                              color="inherit"
+                              aria-label="menu"
+                              sx={{ mr: 2 , display: {xl:"none",lg:"block",md:"block"}}}
+                              href="https://i2kconnect.com"
+                              target="_blank">
+                      <I2KIcon style={{width: 96, height: 96, opacity:"50%"}}/>
+                  </IconButton>
+              </div>
+              <div style={{position: "absolute", bottom: 48, right: 226, width: 226, height: 96}}>
+                  <IconButton size="large"
+                              edge="start"
+                              color="inherit"
+                              aria-label="menu"
+                              sx={{ mr: 2 ,display: {xs:'none',sm:'none',md:'none',lg:'none',xl:'block'}}}
+                              href="https://i2kconnect.com"
+                              target="_blank"
+                              >
+                      <I2KConnectIcon style={{width: 452, height: 96, opacity:"50%"}}/>
+                  </IconButton>
+              </div>
           </Box>
       </Box>
 );
